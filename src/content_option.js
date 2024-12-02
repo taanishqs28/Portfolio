a304506e88cd5e63f1c3e5363115195040d8f9d3
Taanishq <taanishqsethi28@gmail.com>
2024-12-02 13:05:07 -0800

    updated the photo

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "Welcome to my Portfolio.",
     },
     description: "",
-    your_img_url: "https://unsplash.com/photos/bPAng5657eE"
+    your_img_url: "https://images.unsplash.com/photo-1733173293377-0345be593b5f?q=80&w=2730&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 };
 
 const dataabout = {

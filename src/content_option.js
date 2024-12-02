22bf05c6d95ea29c3b80348544988e7b60e109e8
Taanishq <taanishqsethi28@gmail.com>
2024-12-02 13:02:28 -0800

    updated the portfolio

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "Welcome to my Portfolio.",
     },
     description: "",
-    your_img_url: "https://images.unsplash.com/photo-1714107409010-d790bde92f4e?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    your_img_url: "https://unsplash.com/photos/bPAng5657eE"
 };
 
 const dataabout = {

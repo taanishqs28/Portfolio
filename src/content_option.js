64110bb1fda8b2f7a037551c9499e3f5affc9f3a
Taanishq <taanishqsethi28@gmail.com>
2025-03-10 14:54:48 -0700

    fixed a bug

--- src/content_option.js
+++ src/content_option.js
@@ -144,7 +144,7 @@ const dataportfolio = [{
         link:"https://github.com/taanishqs28/BudgetApp",
     },
     {
-        img: "https://plus.unsplash.com/premium_photo-1700830452915-434970100217?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description: "HomeSentinel – AI-Powered Smart Security System",
         link:"https://github.com/taanishqs28/HomeSentinel",
     }
